446d94abe1de9d5b1d5821ca81de5d3c0a7ba41d
Cristiano Bianchi <cristiano.bianchi@gmail.com>
2018-12-03 23:42:52 +0100

    Created Container.tsx

---
+++ src/Container.tsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+//
+interface IDish {
+  name: string;
+  price: number;
+  id: number;
+}
+
+interface IStarters extends IDish {
+  description: string;
+}
+
+interface IMain extends IDish {}
+
+interface IDessert extends IDish {}
+//
+
+interface IContainerState {
+  starters: IStarters[];
+  main: IMain[];
+  dessert: IDessert[];
+}
+
+interface IContainerProps {}
+//
+
+export default class Container extends Component<
+  IContainerProps,
+  IContainerState
+> {
+  constructor(props: IContainerProps) {
+    super(props);
+    this.state = {
+      starters: [{ name: "Prawn Cocktail", price: 10, description: "", id: 1 }],
+      main: [{ name: "Meatball", price: 15, id: 2 }],
+      dessert: [{ name: "Cheesecake", price: 8, id: 3 }]
+    };
+  }
+
+  render(): React.ReactNode {
+    return null;
+  }
+}
